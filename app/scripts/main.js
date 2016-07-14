dcf4edd99b9fdfd89e69b9fc89eee9d6e6d39193
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-07-13 19:12:17 -0500

    bio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,21 @@
 'use strict';
 
-// var projects = document.querySelector('.projects');
+var bio = document.querySelector('.bio-wrapper');
 var tidbits = document.querySelector('.tidbits-wrapper');
 
 var route = function() {
   var destination = location.hash;
-  console.log('yo');
   if (destination.indexOf('tidbits') > -1) {
     tidbits.classList.add('active');
   }
 
+  else if (destination.indexOf('bio') > -1) {
+    bio.classList.add('active');
+  }
+
   else {
     tidbits.classList.remove('active');
+    bio.classList.remove('active');
   }
 };
 
