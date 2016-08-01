13ae6df3366183b20c1f7b356ae326a6354c5cdb
gabedb <gabedb@me.com>
2016-07-31 22:22:56 -0500

    added Tidbits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,9 @@
 var bio = document.querySelector('.bio-wrapper');
 var caravan = document.querySelector('.caravan-wrapper');
 // Change the name accordingly, then uncomment
-var PROJECTNAME;
+var tidbits = document.querySelector('.tidbits-wrapper');
+// PROJECTNAME = document.querySelector('.PROJECT-NAME-wrapper');
+// var PROJECTNAME;
 // PROJECTNAME = document.querySelector('.PROJECT-NAME-wrapper');
 
 var route = function() {
@@ -16,15 +18,15 @@ var route = function() {
     bio.classList.add('active');
   }
 
-  else if (destination.indexOf('PROJECT-NAME') > -1) {
-    PROJECTNAME.classList.add('active');
+  else if (destination.indexOf('tidbits') > -1) {
+    tidbits.classList.add('active');
   }
 
   else {
     caravan.classList.remove('active');
     bio.classList.remove('active');
     // Change name accordingly then uncomment
-    // PROJECTNAME.classList.add('active');
+    tidbits.classList.remove('active');
   }
 };
 
